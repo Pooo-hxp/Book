33b450ee30e16849a8141194ddcbee76d526927d
GitHpotato <hengxipeng@qq.com>
2020-05-14 14:19:29 +0800

    引入简单组件

---
+++ demo01/src/App.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// const Component=React.Component;
+// 等同于
+import React,{Component} from 'react';
+class App extends Component{
+    render(){
+        return(
+                <div>
+                    hello React！
+                </div>
+        )
+    }
+}
+export default App;
+// 把组件暴露出去
---
+++ demo01/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';//引入dom操作方法
+import App from './App';
+
+ ReactDOM.render(<App />,document.getElementById('root')); //把组件挂载到public下root元素下
