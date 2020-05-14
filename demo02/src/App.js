5d664e33010cd2e968400b8e0e9f1d33e90cba80
GitHpotato <hengxipeng@qq.com>
2020-05-14 15:02:02 +0800

    修改原生效果

---
+++ demo02/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const Component=React.Component;
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        hello React!
+      </div>
+    )
+  }
+}
+export default App;
